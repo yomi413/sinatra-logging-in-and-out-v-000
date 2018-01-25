63b58e2bfc1eee44baa63f7a874de209ee4357a9
Yomaira Escano <yjescano@gmail.com>
2018-01-25 06:51:18 +0000

    automatically backed up by learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,12 +1,11 @@
 class Helpers
 
   def self.current_user(session)
-    session["user_id"] = User.new
-    binding.pry
+    @user = User.find_by_id(session[:user_id])
   end
 
   def self.is_logged_in?(session)
-    !!self.current_user(session) ? true : false
+    !!session[:user_id]
   end
 
 end
