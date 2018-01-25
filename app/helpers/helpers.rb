6254421e8aeea343d95032185e8b2568233fdf61
Yomaira Escano <yjescano@gmail.com>
2018-01-25 04:26:14 +0000

    automatically backed up by learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -2,8 +2,8 @@ class Helpers
 
   def self.current_user(session_hash)
     # binding.pry
-    # user = session_hash[:user_id]
-    self
+    session_hash[:user_id] = User.find_by(params[:user_id])
+
 
   end
 
